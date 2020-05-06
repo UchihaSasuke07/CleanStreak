ecbbab350b29c262a90e9ce0ad449e5391df98e1
UchihaSasuke07 <shubhamchaudhary9nov@gmail.com>
2020-05-06 11:34:51 +0530

    push notifications

--- app/src/main/java/com/example/cleanstreak/MainActivity.java
+++ app/src/main/java/com/example/cleanstreak/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.cleanstreak;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +51,22 @@ public class MainActivity extends AppCompatActivity {
         plusBtn.setOnClickListener(clickListener);
         resetBtn = (Button)findViewById(R.id.resetBtn);
         resetBtn.setOnClickListener(clickListener);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("instance-id", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.w("FMX TOKEN", msg);
+
+                    }
+                });
 
         initCounter();
     }
